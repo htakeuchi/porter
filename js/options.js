00f9d06a78a874bf621baf10b3daf85ef0161c4e
TAKEUCHI, Hitoshi <hitoshi@namaraii.com>
2016-02-03 16:01:45 +0900

    change textarea to CodeMirror

--- js/options.js
+++ js/options.js
@@ -1,21 +1,32 @@
 (function() {
+  var editor;
+  
   function save() {    
     // Custom CSS
-    chrome.storage.sync.set({'css': document.getElementById('textArea').value});    
+    chrome.storage.sync.set({'css': editor.getValue()});    
   };
   
   function load() {
     chrome.storage.sync.get("css", function(css) {
       if (!chrome.runtime.error) {
         console.log(css);
-        document.getElementById("textArea").value = css.css;
+        editor.setValue(css.css);
       }
     });
   };
 
   function main() {
-    load();
     document.getElementById("save").addEventListener("click",  function() { save(); }, false);
+    
+    var textArea = document.getElementById("textArea");
+    editor = CodeMirror.fromTextArea(textArea, {
+      mode: "css",
+      value: "",
+      lineNumbers: true,
+      extraKeys: {"Ctrl-Space": "autocomplete"},
+      tabSize: 2
+    });
+    load();
   };
 
   main();
